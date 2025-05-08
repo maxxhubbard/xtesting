39eae0251b3b377b679f0d9973c7470ecf3757b2
maxxhubbard <max_xhubbard@outlook.com>
2025-05-08 13:33:24 +0100

    Update raw_gitlab_uc.py

--- raw_gitlab_uc.py
+++ raw_gitlab_uc.py
@@ -1,14 +1,13 @@
 from seleniumbase import SB
+import time
+
 
 with SB(uc=True, test=True) as sb:
-    url = "https://gitlab.com/users/sign_in"
+    url = "https://kick.com/brutalles"
     sb.uc_open_with_reconnect(url, 5)
     sb.uc_gui_click_captcha()
     sb.sleep(2)
     sb.uc_gui_handle_captcha()
-    sb.assert_text("Username", '[for="user_login"]', timeout=3)
-    sb.assert_element('label[for="user_login"]')
-    sb.highlight('button:contains("Sign in")')
-    sb.highlight('h1:contains("GitLab.com")')
-    sb.post_message("SeleniumBase wasn't detected", duration=4)
-    print("Success! Website did not detect SeleniumBase!")
+    start_time = time.time()
+    duration = 100 * 60
+
